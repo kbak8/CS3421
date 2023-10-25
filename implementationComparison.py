706d9a914b1549e615080fe1cb3bdd0652a358cf
Patrick Janssen <pdjansse@mtu.edu>
2023-10-25 12:59:31 -0400

    spilt fuctions and added way to change amount of runs per combo

--- implementationComparison.py
+++ implementationComparison.py
@@ -24,50 +24,43 @@ def insertion_sort(arr):
         arr[j + 1] = key
 
 def benchmark(args):
-    n, m = args
+    n, m, f, num_runs = args
     pid = os.getpid()
-    print(f"[Process {pid}] Starting benchmark for n={n}, m={m}")
+    print(f"[Process {pid}] Starting {f.__name__} benchmark for n={n}, m={m}, runs={num_runs}")
 
-    total_time_bubble = 0
-    total_time_insertion = 0
-    
-    for _ in range(1000):
+    total_time = 0
+    for _ in range(num_runs):
         arr = generate_random_array(n, m)
-        arr_copy_for_bubble = arr.copy()
-        arr_copy_for_insertion = arr.copy()
+        time_bubble = timeit.timeit(lambda: f(arr), number=1)
+        total_time += time_bubble
 
-        time_bubble = timeit.timeit(lambda: bubble_sort(arr_copy_for_bubble), number=1)
-        total_time_bubble += time_bubble
-
-        time_insertion = timeit.timeit(lambda: insertion_sort(arr_copy_for_insertion), number=1)
-        total_time_insertion += time_insertion
-
-    avg_time_bubble = total_time_bubble       # (total_time_bubble / 1000) * 1000 Finds the avg then Convert to milliseconds
-    avg_time_insertion = total_time_insertion # (total_time_bubble / 1000) * 1000 Finds the avg then Convert to milliseconds
+    avg_time = (total_time / num_runs) * 1000  # Convert to milliseconds
     
-    print(f"[Process {pid}] Completed benchmark for n={n}, m={m}.")
-    return avg_time_bubble, avg_time_insertion
+    print(f"[Process {pid}] Completed {f.__name__} benchmark for n={n}, m={m}. Average time: {avg_time:.2f} ms")
+    return n, m, f.__name__, avg_time 
 
 
 if __name__ == "__main__":
     n_values = [1000, 10000, 100000]
     m_values = [500, 5000, 50000]
+    functions = [bubble_sort, insertion_sort]
+    num_runs_dict = {n_values[0]: 1000, n_values[1]: 1000, n_values[2]: 10}  # Number of runs for each n value
 
     # If you have more than 2 cores, use all but two of them
-    pool = ProcessPool(processes=max(1,os.cpu_count() - 2))
-    print(f"Total CPU cores available: {os.cpu_count()} cores Using: {max(1,os.cpu_count() - 2)} cores")
+    pool = ProcessPool(processes=max(1, os.cpu_count() - 2))
+    print(f"Total CPU cores available: {os.cpu_count()} cores Using: {max(1, os.cpu_count() - 2)} cores")
     
-    args_list = [(n, m) for n in n_values for m in m_values]
+    args_list = [(n, m, f, num_runs_dict[n], ) for n in n_values for m in m_values for f in functions] 
     results = pool.map(benchmark, args_list)
 
     print("Benchmarking completed!")
 
-    bubble_sort_results = [res[0] for res in results]
-    insertion_sort_results = [res[1] for res in results]
+    bubble_sort_results = [res[3] for res in results if res[2] == "bubble_sort"]
+    insertion_sort_results = [res[3] for res in results if res[2] == "insertion_sort"]
 
     plt.figure(figsize=(10, 6))
 
-    x_ticks_labels = [f"{n}, {m}" for n in n_values for m in m_values]
+    x_ticks_labels = [f"{n}, {m}, runs:{num_runs_dict[n]}" for n in n_values for m in m_values]
     x_ticks_positions = range(len(x_ticks_labels))
 
     plt.plot(x_ticks_positions, bubble_sort_results, label='Bubble Sort', marker='o')
