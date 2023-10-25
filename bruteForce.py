8634c1a3486e8078971c025d081f3e378382438b
Ethan Jones <egjones@mtu.edu>
2023-10-24 23:33:27 -0400

    corrected m_values and added print statements for data

--- bruteForce.py
+++ bruteForce.py
@@ -8,7 +8,7 @@ def generate_random_array(n, m):
 
 def is_unique(arr):
     comparisons = 0
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         for j in range(i + 1, len(arr)):
             comparisons += 1
             if arr[i] == arr[j]:
@@ -16,10 +16,13 @@ def is_unique(arr):
     return True, comparisons
 
 
-m_values = [100, 200, 300, 400, 200, 3100]
-n_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+m_values = [1000, 10000] # Maximum m-values for each array are 1000 and 10000. (Group 10 -> (100*10, 1000*10))
+n_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] # Determine length of array. (n_factors[i] * m = length)
+
+print("Average comparisons after 1000 runs:")
 
 for m in m_values:
+    index = 0
     average_comparisons = []
     for n_factor in n_factors:
         n = int(n_factor * m)
@@ -29,6 +32,8 @@ for m in m_values:
             is_unique_result, comparisons = is_unique(arr)
             total_comparisons += comparisons
         average_comparisons.append(total_comparisons / 1000)
+        print("Maximum m-value: " + str(m) + ", Length: " + str(n) + ", Average comparisons: " + str(average_comparisons[index]))
+        index += 1
 
     plt.plot([n_factor * m for n_factor in n_factors], average_comparisons, label=f'm={m}')
 
